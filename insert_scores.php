89ff0609afb937024a96d9f5ffeaa4bac3c4341c
Omer Kremer <omer@betterhelp.com>
2019-08-25 16:46:49 -0700

    Fix composer autoload path

--- insert_scores.php
+++ insert_scores.php
@@ -1,5 +1,5 @@
 <?php
-require_once '/path/to/vendor/autoload.php';
+require_once 'vendor/autoload.php';
 
 $loader = new \Twig\Loader\FilesystemLoader('/');
 $twig = new \Twig\Environment($loader, [
